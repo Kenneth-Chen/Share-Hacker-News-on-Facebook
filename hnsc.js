858c4a9327fe5ecc85664e275ddd68a2af6928d4
Kenneth Chen <kennchen@gmail.com>
2013-08-13 23:15:44 -0400

    fix for comments

--- hnsc.js
+++ hnsc.js
@@ -17,7 +17,7 @@ function init() {
         var url = window.location.origin + "/" + encodeURIComponent($(this).attr('href'));
         var fbscript = " | <a rel='nofollow' href='http://www.facebook.com/sharer/sharer.php?s=100&u=" + url + "' onclick=\"(function(e){u='" + url + "';t=document.title;window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;})(event)\" target='_blank'>fb</a>";
         var parent = $(this).parent();
-        if(parent.hasClass("subtext")) {
+        if(parent.hasClass("subtext") || parent.hasClass("comhead")) {
             parent.append(fbscript);
         } else {
             parent.parent().next().find(".subtext").append(fbscript);
